60e52c6360c0998ac6b42bfd5fafb939e033bb06
Waswa Rodgers <waswam1994@gmail.com>
2020-07-19 07:22:08 +0300

    Deleted unimportant files

--- testingAgain/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server="localhost";
-$user="root";
-$password="";
-$database="testify";
-
-$connection=mysqli_connect($server, $user, $password, $database) or die("Database connection error:".mysqli_connect_error());
- ?>
